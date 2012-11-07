6dfc0de98a24fe96f45e25c7d8fa0d427093a407
Jarek Plonski <jaroslaw.plonski@gmail.com>
2012-11-07 23:08:51 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarBreak::Application.config.secret_token = 'bc7a6fe1ced7cdac81e8b18fdf27cc3a89eed6ddb5aafed7cd2ec1f06f1009319a0d044b3b6929f2a062abf39a84545fbc5493fb4505f043b87bdee5eb719215'
